647039a8fe87f348392f90435d965f5d37b3b5d3
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-10-19 00:45:05 -0300

    :lipstick: style: adiciona status bar com estilo claro

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
 import Header from "@components/layout/header";
+import { StatusBar } from "expo-status-bar";
 
 function Home() {
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
       <Header />
     </SafeAreaView>
   );
