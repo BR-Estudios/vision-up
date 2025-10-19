a5a177dbf2468f61c80073fd2849c9440ab7aa38
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-10-19 12:06:20 -0300

    :sparkles: feat: desenvolvido componente de abas de navegação

---
+++ src/data/tabs/index.ts
@@ -0,0 +1,24 @@
+import { translation } from '@utils/locale';
+
+export const tabs: Tab[] = [
+  {
+    name: 'employees',
+    icon: 'robot-industrial',
+    label: translation('tab.employees'),
+  },
+  {
+    name: 'daily',
+    icon: 'book-music',
+    label: translation('tab.daily'),
+  },
+  {
+    name: 'surgery',
+    icon: 'hospital-box',
+    label: translation('tab.surgery'),
+  },
+  {
+    name: 'store',
+    icon: 'store',
+    label: translation('tab.store'),
+  },
+];
